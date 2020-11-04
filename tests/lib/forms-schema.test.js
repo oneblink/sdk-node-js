f593cbf535113b10e6aa4fa078a974e6c72b0f4d
Zac Turner <zac@oneblink.io>
2020-11-04 16:16:39 +1100

    ON-8840 # Added test

--- tests/lib/forms-schema.test.js
+++ tests/lib/forms-schema.test.js
@@ -4657,7 +4657,7 @@ describe('submission event conditional logic', () => {
     )
     expect(result.error).toBe(null)
   })
-  test.only('should reject element with missing property in between conditional logic', () => {
+  test('should reject element with missing property in between conditional logic', () => {
     const result = Joi.validate(
       {
         id: 1,
@@ -4710,6 +4710,62 @@ describe('submission event conditional logic', () => {
 
     expect(result.error.message).toContain(
       '"Form Element - Conditionally Show Predicate - Between-Max" is required',
+    )
+  })
+  test('should reject element with conditional between predicate where max is lower than min', () => {
+    const result = Joi.validate(
+      {
+        id: 1,
+        name: 'conditionally show element via number input',
+        description: 'string',
+        formsAppEnvironmentId: 1,
+        formsAppIds: [1],
+        organisationId: 'ORGANISATION_00000000001',
+        postSubmissionAction: 'FORMS_LIBRARY',
+        isMultiPage: false,
+        elements: [
+          {
+            id: 'b941ea2d-965c-4d40-8c1d-e5a231fc18b1',
+            name: 'Numbers',
+            label: 'Numbers',
+            type: 'number',
+            required: false,
+            minNumber: 1,
+            maxNumber: 6,
+            defaultValue: 3,
+          },
+          {
+            id: '8e4d819b-97fa-438d-b613-a092d38c3b23',
+            name: 'Text',
+            label: 'Text',
+            type: 'text',
+            required: false,
+            defaultValue: 'text',
+            isDataLookup: false,
+            isElementLookup: false,
+            readOnly: false,
+            conditionallyShow: true,
+            requiresAllConditionallyShowPredicates: false,
+            conditionallyShowPredicates: [
+              {
+                elementId: 'b941ea2d-965c-4d40-8c1d-e5a231fc18b1',
+                type: 'BETWEEN',
+                min: 8,
+                max: 6,
+              },
+            ],
+          },
+        ],
+        isAuthenticated: true,
+      },
+      formSchema,
+      {
+        abortEarly: false,
+      },
+    )
+
+    expect(result.error.message).toContain(
+      '"Form Element - Conditionally Show Predicate - Between-Max" must be larger than or equal to 8',
     )
   })
 })
