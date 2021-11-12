bd09c3bd12cde83a2a0e9ea9a4cda8a8405c8b71
kizaonline <kizaonline@gmail.com>
2021-11-12 17:06:23 +1100

    ON-16776 # Update test

--- tests/organisations.test.ts
+++ tests/organisations.test.ts
@@ -8,6 +8,7 @@ describe('Organisations SDK Class', () => {
 
   describe('getOrganisation', () => {
     test('should reject with correct validation errors for "organisationId"', () => {
+      // @ts-expect-error Testing validation
       return expect(organisations.getOrganisation()).rejects.toThrow(
         'Must supply "organisationId" as a string',
       )
