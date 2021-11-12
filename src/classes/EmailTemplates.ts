db450a7b3abe52da47ca41751e190654a811130d
kizaonline <kizaonline@gmail.com>
2021-11-12 15:06:25 +1100

    ON-16700 # Update to TSDocs, include class in tenants

--- src/classes/EmailTemplates.ts
+++ src/classes/EmailTemplates.ts
@@ -1,63 +1,150 @@
 import OneBlinkAPI from '../lib/one-blink-api'
-import {
-  EmailTemplateTypes,
-} from '@oneblink/types'
-import {
-  BaseSearchResult,
-  Tenant,
-  ConstructorOptions,
-} from '../lib/types'
+import { EmailTemplateTypes } from '@oneblink/types'
+import { BaseSearchResult, ConstructorOptions } from '../types'
 
 type EmailTemplatesSearchResult = {
   formsAppEnvironments: EmailTemplateTypes.EmailTemplate[]
 } & BaseSearchResult
 
 type EmailTemplatesSearchOptions = {
-  formsAppEnvironmentId: number;
+  formsAppEnvironmentId: number
   limit?: number
   offset?: number
 }
 
 const basePath = `/email-templates`
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-export default (tenant: Tenant) =>
-  class EmailTemplates extends OneBlinkAPI {
-    constructor(options: ConstructorOptions) {
-      options = options || {}
-      super(options.accessKey, options.secretKey, tenant)
-    }
-
-    searchEmailTemplates(searchParams?: EmailTemplatesSearchOptions): Promise<EmailTemplatesSearchResult> {
-      return super.searchRequest(basePath, searchParams)
-    }
-
-    async getEmailTemplate(id?: number): Promise<EmailTemplateTypes.EmailTemplate> {
-      if (typeof id !== 'number') {
-        throw new TypeError('Must supply "id" as a number')
-      }
-
-      return super.searchRequest(`${basePath}/${id}`)
-    }
-
-    async createEmailTemplate(data?: unknown): Promise<EmailTemplateTypes.EmailTemplate> {
-      return super.postRequest(basePath, data)
-    }
-
-    async updateEmailTemplate(
-      data?: Record<string, unknown>,
-    ): Promise<EmailTemplateTypes.EmailTemplate> {
-      if (!data || typeof data.id !== 'number') {
-        throw new TypeError('Must supply "EmailTemplate.id" as a number')
-      }
-
-      return super.putRequest(`${basePath}/${data.id}`, data)
-    }
-
-    async deleteEmailTemplate(id?: unknown): Promise<void> {
-      if (typeof id !== 'number') {
-        throw new TypeError('Must supply "id" as a number')
-      }
-
-      return super.deleteRequest(`${basePath}/${id}`)
-    }
+export default class EmailTemplates extends OneBlinkAPI {
+  /**
+   * Example
+   *
+   * ```typescript
+   * const OneBlink = require('@oneblink/sdk')
+   *
+   * const options = {
+   *   accessKey: '123455678901ABCDEFGHIJKL',
+   *   secretKey: '123455678901ABCDEFGHIJKL123455678901ABCDEFGHIJKL',
+   * }
+   * const emailTemplates = new OneBlink.EmailTemplates(options)
+   * ```
+   */
+  constructor(options: ConstructorOptions) {
+    options = options || {}
+    super(options.accessKey, options.secretKey)
   }
+
+  /**
+   * Example
+   *
+   * ```javascript
+   * const searchParams = {
+   *   formsAppEnvironmentId: 1,
+   *   limit: 1,
+   *   offset: 0,
+   * }
+   * const { emailTemplates, meta } =
+   *   await emailTemplates.searchEmailTemplates(searchParams)
+   * ```
+   *
+   * @param searchParams Search options
+   * @returns
+   */
+  searchEmailTemplates(
+    searchParams?: EmailTemplatesSearchOptions,
+  ): Promise<EmailTemplatesSearchResult> {
+    return super.searchRequest(basePath, searchParams)
+  }
+
+  /**
+   * Example
+   *
+   * ```javascript
+   * const emailTemplate = await emailTemplates.getEmailTemplate(1)
+   * // Use data here...
+   * ```
+   *
+   * @param id The id of the form submission approval
+   * @returns
+   */
+  async getEmailTemplate(
+    id?: number,
+  ): Promise<EmailTemplateTypes.EmailTemplate> {
+    if (typeof id !== 'number') {
+      throw new TypeError('Must supply "id" as a number')
+    }
+
+    return super.searchRequest(`${basePath}/${id}`)
+  }
+
+  /**
+   * Example
+   *
+   * ```javascript
+   * const data = {
+   *   name: 'my template'
+   *   template: 'My email template {{custom:my-custom-tag}}'
+   *   formsAppEnvironmentId: 1
+   *   type: 'FORM_SUBMISSION_EVENT_PDF'
+   * }
+   * const emailTemplate = await emailTemplates.createEmailTemplate(
+   *   data,
+   * )
+   * // Use data here...
+   * ```
+   *
+   * @param data The data for the new email template
+   * @returns
+   */
+  async createEmailTemplate(
+    data?: unknown,
+  ): Promise<EmailTemplateTypes.EmailTemplate> {
+    return super.postRequest(basePath, data)
+  }
+
+  /**
+   * Example
+   *
+   * ```javascript
+   * const data = {
+   *   id: 1,
+   *   name: 'my updated template'
+   *   template: 'My email template {{custom:my-custom-tag}}'
+   *   formsAppEnvironmentId: 1
+   *   type: 'FORM_SUBMISSION_EVENT_PDF'
+   * }
+   * const emailTemplate = await emailTemplates.updateEmailTemplate(
+   *   data,
+   * )
+   * // Use data here...
+   * ```
+   *
+   * @param data The data for the new email template
+   * @returns
+   */
+  async updateEmailTemplate(
+    data?: Record<string, unknown>,
+  ): Promise<EmailTemplateTypes.EmailTemplate> {
+    if (!data || typeof data.id !== 'number') {
+      throw new TypeError('Must supply "EmailTemplate.id" as a number')
+    }
+
+    return super.putRequest(`${basePath}/${data.id}`, data)
+  }
+
+  /**
+   * Example
+   *
+   * ```javascript
+   * await emailTemplates.deleteEmailTemplate(1)
+   * ```
+   *
+   * @param id The id of the form submission approval
+   * @returns
+   */
+  async deleteEmailTemplate(id?: unknown): Promise<void> {
+    if (typeof id !== 'number') {
+      throw new TypeError('Must supply "id" as a number')
+    }
+
+    return super.deleteRequest(`${basePath}/${id}`)
+  }
+}
