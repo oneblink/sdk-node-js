e2dc4de1c70f6744011bb5ff1f18dcd4cddd58e4
Zac Turner <zac@oneblink.io>
2021-11-15 09:59:41 +1100

    ON-16779 # Documented TeamMember class

--- src/classes/TeamMembers.ts
+++ src/classes/TeamMembers.ts
@@ -2,16 +2,41 @@ import { TeamMemberTypes } from '@oneblink/types'
 import OneBlinkAPI from '../lib/one-blink-api'
 import { ConstructorOptions } from '../types'
 
-// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default class TeamMembers extends OneBlinkAPI {
+  /**
+   * #### Example
+   *
+   * ```typescript
+   * const OneBlink = require('@oneblink/sdk')
+   *
+   * const options = {
+   *   accessKey: '123455678901ABCDEFGHIJKL',
+   *   secretKey: '123455678901ABCDEFGHIJKL123455678901ABCDEFGHIJKL',
+   * }
+   * const teamMembersSDK = new OneBlink.TeamMembers(options)
+   * ```
+   */
   constructor(options: ConstructorOptions) {
     options = options || {}
     super(options.accessKey, options.secretKey)
   }
 
-  async getTeamMemberRole(
-    email?: unknown,
-  ): Promise<TeamMemberTypes.Role | null> {
+  /**
+   * #### Example
+   *
+   * ```javascript
+   * const email = 'email@domain.io'
+   * teamMembersSDK.getTeamMemberRole(email).then((role) => {
+   *   if (role !== null) {
+   *     // Use role here...
+   *   }
+   * })
+   * ```
+   *
+   * @param keyId The email address the team member uses to login
+   * @returns
+   */
+  async getTeamMemberRole(email: string): Promise<TeamMemberTypes.Role | null> {
     if (typeof email !== 'string') {
       return Promise.reject(new TypeError('Must supply "email" as a string'))
     }
