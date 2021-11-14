12985c94f2154c4459da420e579a181e4dba0b44
josh215 <josh@oneblink.io>
2021-11-15 09:32:54 +1100

    ON-16778 # Added TSDoc to sendEmail.

--- src/classes/sendEmail.ts
+++ src/classes/sendEmail.ts
@@ -8,7 +8,37 @@ export {
   SendMailOptions as SendEmailOptions,
   SentMessageInfo as SendEmailResult,
 }
-
+  /**
+   * #### Example
+   *
+   * ```javascript
+   * const OneBlink = require('@oneblink/sdk')
+   *
+   * await OneBlink.sendEmail({
+   *   subject: 'Message title',
+   *   html: '<p>HTML version of the message</p>',
+   *   from: {
+   *     address: 'noreply@example.com',
+   *     name: 'No Reply',
+   *   },
+   *   to: ['email@example.com'],
+   *   cc: ['you@example.com'],
+   *   bcc: ['not-you@example.com'],
+   *   attachments: [
+   *     {
+   *       filename: pdfFileName,
+   *       content: pdfData,
+   *       contentType: 'application/pdf',
+   *       encoding: 'binary',
+   *     },
+   *   ],
+   * })
+   * ```
+   * 
+   * ### Options
+   *
+   * This function uses the popular [`nodemailer`](https://www.npmjs.com/package/nodemailer) internally to send emails. Please see the documentation for [Message Configuration](https://nodemailer.com/message/) for available options.
+   */
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 export default (tenant: Tenant) =>
   async function sendEmail(options: SendMailOptions): Promise<SentMessageInfo> {
