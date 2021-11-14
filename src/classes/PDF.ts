8d36791c41b7a909a6113d32c4c0d717afecb752
josh215 <josh@oneblink.io>
2021-11-15 10:32:00 +1100

    ON-16777 # Added TSDoc to PDF.ts

--- src/classes/PDF.ts
+++ src/classes/PDF.ts
@@ -4,11 +4,45 @@ import OneBlinkAPI from '../lib/one-blink-api'
 import { ConstructorOptions } from '../types'
 
 export default class PDF extends OneBlinkAPI {
+  /**
+   * #### Example
+   *
+   * ```typescript
+   * const OneBlink = require('@oneblink/sdk')
+   *
+   * const options = {
+   *   accessKey: '123455678901ABCDEFGHIJKL',
+   *   secretKey: '123455678901ABCDEFGHIJKL123455678901ABCDEFGHIJKL',
+   * }
+   * const keys = new OneBlink.Keys(options)
+   * ```
+   */
   constructor(options: ConstructorOptions) {
     options = options || {}
     super(options.accessKey, options.secretKey)
   }
 
+  /**
+   * ```javascript
+   * const fs = require('fs')
+   * const util = require('util')
+   * 
+   * const writeFileAsync = util.promisify(fs.writeFile)
+   * 
+   * async function run() {
+   *     const buffer = await pdf.generateFormSubmissionPDF({
+   *     formId: 1,
+   *     submissionId: 'c63ec3ac-12ab-447c-951c-2815d0e6fc24',
+   *     isDraft: false,
+   *     includeSubmissionIdInPdf: false,
+   *     excludedElementIds: ['1ae6d5f5-eade-411c-b85a-45fe40fe469e'],
+   *   })
+   *   await writeFileAsync('./submission.pdf', buffer, 'binary')
+   * }
+   * ```
+   * @param options.formId The exact identifer of the form you wish to generate a pdf for 
+   * 
+   */
   async generateFormSubmissionPDF(options: {
     formId: number
     submissionId: string
@@ -58,6 +92,60 @@ export default class PDF extends OneBlinkAPI {
     return response.buffer()
   }
 
+  
+  /**
+   * #### Example
+   * ```javascript
+   * const fs = require('fs')
+   * const util = require('util')
+   * 
+   * const writeFileAsync = util.promisify(fs.writeFile)
+   * 
+   * async function run() {
+   *   const buffer = await pdf.generatePDF({
+   *     body: {
+   *       html: `
+   *         <p>I will be in the middle</p>
+   *       `,
+   *     },
+   *     header: {
+   *       html: `
+   *       <div style="font-size: 9px; margin: 0 15px; width: 100%; text-align: center;">
+   *         I will be at the top of every page
+   *       </div>
+   *       `,
+   *     },
+   *     footer: {
+   *       html: `
+   *       <div style="font-size: 9px; margin: 0 15px; width: 100%; text-align: center;">
+   *         I will be at the bottom of every page ({_BLINK_PAGE_NO_}/{_BLINK_PAGES_})
+   *       </div>
+   *       `,
+   *     },
+   *     page: {
+   *       orientation: 'Portrait',
+   *       size: 'A4',
+   *       margins: {
+   *         top: '15mm',
+   *         right: '5mm',
+   *         bottom: '15mm',
+   *         left: '5mm',
+   *       },
+   *     },
+   *   })
+   *   await writeFileAsync('./custom.pdf', buffer, 'binary')
+   * }
+   * 
+   * @param options.body.html The HTML to render as a pdf
+   * @param options.header.html The HTML to render at the top of every page
+   * @param options.footer.html The HTML to render at the bottom of every page
+   * @param options.page.orientation `'Portrait'` or `'Landscape`'. Default is `'Portrait'`
+   * @param options.page.size `'Letter'`, `'Legal'`, `'Tabloid'`, `'Ledger'`, `'A0'`, `'A1'`, `'A2'`, `'A3'`, `'A4'`, `'A5'` or `'A6'`. Default is `'A4'`
+   * @param options.page.margins.top How much space between the top of each page and the content. Supported dimension units are: `'mm'`, `'cm'`, `'in'`, `'px'` 
+   * @param options.page.margins.bottom How much space between the bottom of the page and content
+   * @param options.page.margins.right How much space between the right of each page and the content. Supported dimension units are: `'mm'`, `'cm'`, `'in'`, `'px'`
+   * @param options.page.margins.left How much space between the left of each page and the content. Supported dimension units are: `'mm'`, `'cm'`, `'in'`, `'px'`
+   * ``` */
   async generatePDF(options: PDFTypes.GeneratePDFOptions): Promise<Buffer> {
     if (!options || !options.body || !options.body.html) {
       throw new TypeError('Must supply "options.body.html" as a string')
