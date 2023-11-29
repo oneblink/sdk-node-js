36d36e13840b9348b5e1a9674def07f1b2a1069c
josh215 <josh@oneblink.io>
2023-11-29 13:34:48 +1100

    ON-36699 # Encoding uri of asset returned after asset upload

--- src/lib/upload-asset.ts
+++ src/lib/upload-asset.ts
@@ -39,6 +39,8 @@ export default async function uploadAsset<T>(
 
   await managedUpload.done()
   return {
-    Location: `https://s3.${assetCredentials.s3.region}.amazonaws.com/${assetCredentials.s3.bucket}/${assetCredentials.s3.key}`,
+    Location: `https://s3.${assetCredentials.s3.region}.amazonaws.com/${
+      assetCredentials.s3.bucket
+    }/${encodeURI(assetCredentials.s3.key)}`,
   }
 }
