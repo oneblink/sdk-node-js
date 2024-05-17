230d352b3d891aee00915331d67034442a189409
jdawg093 <49189518+jdawg093@users.noreply.github.com>
2024-05-17 17:01:08 +1000

    ON-40444 # Added google address validation

---
+++ src/lib/forms-schema/elements/GoogleAddressElement.ts
@@ -0,0 +1,30 @@
+import Joi from 'joi'
+import {
+  baseSchemas,
+  name,
+  label,
+  hint,
+  requiredSchemas,
+  readOnly,
+  conditionallyShowSchemas,
+  placeholderValue,
+  lookupSchemas,
+  customCssClasses,
+  hintPosition,
+} from '../property-schemas'
+
+export const type = 'googleAddress'
+
+export default Joi.object({
+  ...baseSchemas,
+  name,
+  label,
+  hint,
+  hintPosition,
+  ...requiredSchemas,
+  readOnly,
+  ...conditionallyShowSchemas,
+  ...lookupSchemas,
+  placeholderValue,
+  customCssClasses,
+})
